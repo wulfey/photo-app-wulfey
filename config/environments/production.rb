298bcba8df7423abe2117b5bb39884d689cf5df2
Jason Wolfe <jasonwolfe@gmail.com>
2017-02-27 03:44:22 +0000

    fixed up environment variables problem on local

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.action_mailer.delivery_method = :smtp 
-  config.action_mailer.default_url_options = { :host => 'photo-apple.herokuapp.com', :protocol => 'https'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host =>  'photo-apple.herokuapp.com', :protocol => 'https'}
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
